ee6f9cd7ba54528852d8245dd3df23e166dc3a1c
adam147g <87074335+adam147g@users.noreply.github.com>
2022-05-26 11:22:32 +0200

    Update Cards.cpp

--- Cards.cpp
+++ Cards.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 class Cards{
+public:
+    Cards(std::string name, std::string suit, std::size_t value, std::size_t id):
+    name_(name), suit_(suit), value_(value), id_(id){}
+
 private:
     std::string name_;
     std::string suit_;
