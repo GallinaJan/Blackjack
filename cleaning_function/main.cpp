0ccc45be06d0a01f946d9ff1e8dcd0721908394c
adam147g <87074335+adam147g@users.noreply.github.com>
2022-05-26 11:13:36 +0200

    Add files via upload

---
+++ cleaning_function/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void cleaning_function(Croupier& croupier, Player& player) {
+    show_current_status(croupier, player)
+    croupier.clear_table
+    croupier.clear_cards
+    player.clear_cards
+    continue
+}
