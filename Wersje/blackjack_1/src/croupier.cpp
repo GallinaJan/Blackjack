6fe924dfa10df5fcc19df3dd53522b985c2f16e4
Jan Gallina <jan.gallina@zhp.net.pl>
2022-06-02 14:56:12 +0200

    ukończona metoda give_croupier_sum

--- Wersje/blackjack_1/src/croupier.cpp
+++ Wersje/blackjack_1/src/croupier.cpp
@@ -21,11 +21,46 @@ bool Croupier::has_ace_as_first()
 int Croupier::give_croupier_sum()
 {
     int ace_number = 0;
+    int sum = 0;
     for(auto elem : croupier_cards_)
     {
         if(elem.id_ == 13 || elem.id_ ==26 ||elem.id_ ==39|| elem.id_ == 52)
         {
         ace_number +=1;
+        }
+    }
+    for(auto elem:croupier_cards_)
+    {
+        if(elem.id_ != 13 && elem.id_ !=26 && elem.id_ !=39 && elem.id_ != 52) {
+            if (
+                    elem.id_ == 10 ||
+                    elem.id_ == 11 ||
+                    elem.id_ == 12 ||
+                    elem.id_ == 23 ||
+                    elem.id_ == 24 ||
+                    elem.id_ == 25 ||
+                    elem.id_ == 36 ||
+                    elem.id_ == 37 ||
+                    elem.id_ == 38 ||
+                    elem.id_ == 49 ||
+                    elem.id_ == 50 ||
+                    elem.id_ == 51 ||
+                    ) {
+                sum += 10;
+            } else {
+                sum += 1 + (elem.id_) % 13;
+            }
+        }
+        if(ace_number != 0)
+        {
+            if((sum +10 + ace_number) <= 21)
+            {
+                sum += (10 + ace_number) ;
+            }
+            else
+            {
+                sum += ace_number;
+            }
         }
     }
 }
