86e212b5587f0da4878a12b45c87e98622ae8f89
Jan Gallina <jan.gallina@zhp.net.pl>
2022-06-02 12:07:16 +0200

    metoda has_ace_as_first

--- Wersje/blackjack_1/src/croupier.cpp
+++ Wersje/blackjack_1/src/croupier.cpp
@@ -4,3 +4,16 @@
 #include <map>
 #include <vector>
 #include "cards.hpp"
+
+bool Croupier::has_ace_as_first()
+{
+    std::string name Cards::give_name(croupier_cards_[0]);
+    if(name == "as" || name == "As")
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
