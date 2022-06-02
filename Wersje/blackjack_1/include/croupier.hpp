cac343f0c9c29968f5c85745090fb2d0a8f8d608
Jan Gallina <jan.gallina@zhp.net.pl>
2022-06-02 11:58:29 +0200

    aktualizuje plik croupier.hpp

--- Wersje/blackjack_1/include/croupier.hpp
+++ Wersje/blackjack_1/include/croupier.hpp
@@ -6,13 +6,15 @@
 class Croupier
 {
 public:
-    void add_money(double added) {money_on_table_ = added;}
-    void give_card() {croupier_cards_.push_back(Card);}
-    void show_second() {std::cout<< croupier_cards_[1] << std::endl;}
+void add_money(double added) {money_on_table_ = added;}
+void give_card() {croupier_cards_.push_back(Card);}
+void show_second() {std::cout<< cruupier_cards_[1] << std::endl;}
+bool has_ace_as_first();
+
 private:
-    double money_on_table_;
-    double sum_ = 0;
-    std::vector<Cards> croupier_cards_;
+double money_on_table_
+double sum_ = 0
+std::vector<Cards> croupier_cards_
 };
 
 #endif //*INCLUDE_CROUPIER_HPP_
