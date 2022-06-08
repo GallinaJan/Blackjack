cba2bcd4def223ed9ac3f7ab14a0d6ce4165f1d2
adam147g <87074335+adam147g@users.noreply.github.com>
2022-06-08 10:19:43 +0200

    Update cards.hpp

--- cards.hpp
+++ cards.hpp
@@ -56,8 +56,8 @@ public:
         }
     }
 
-    Cards take_card(bool *need_to_shuffle){
-        if(hand_.size() > 10){
+    static Cards take_card(bool *need_to_shuffle){
+        if(hand_.size() < 20){
             *need_to_shuffle = true;
         }
         if(hand_.size() > 0) {
@@ -71,72 +71,72 @@ public:
     }
 
 private:
-    std::vector<Cards> hand_;
+    inline static std::vector<Cards> hand_;
     Cards default_tab_[52] = {
-            Cards("as", "kier", 11, 0),
-            Cards("as", "karo", 11, 1),
-            Cards("as", "pik", 11, 2),
-            Cards("as", "trefl", 11, 3),
+            Cards("Ace", "Hearts", 11, 0),
+            Cards("Ace", "Diamonds", 11, 1),
+            Cards("Ace", "Spades", 11, 2),
+            Cards("Ace", "Clubs", 11, 3),
 
-            Cards("krol", "kier", 10, 4),
-            Cards("krol", "karo", 10, 5),
-            Cards("krol", "pik", 10, 6),
-            Cards("krol", "trefl", 10, 7),
+            Cards("King", "Hearts", 10, 4),
+            Cards("King", "Diamonds", 10, 5),
+            Cards("King", "Spades", 10, 6),
+            Cards("King", "Clubs", 10, 7),
 
-            Cards("dama", "kier", 10, 8),
-            Cards("dama", "karo", 10, 9),
-            Cards("dama", "pik", 10, 10),
-            Cards("dama", "trefl", 10, 11),
+            Cards("Queen", "Hearts", 10, 8),
+            Cards("Queen", "Diamonds", 10, 9),
+            Cards("Queen", "Spades", 10, 10),
+            Cards("Queen", "Clubs", 10, 11),
 
-            Cards("walet", "kier", 10, 12),
-            Cards("walet", "karo", 10, 13),
-            Cards("walet", "pik", 10, 14),
-            Cards("walet", "trefl", 10, 15),
+            Cards("Jack", "Hearts", 10, 12),
+            Cards("Jack", "Diamonds", 10, 13),
+            Cards("Jack", "Spades", 10, 14),
+            Cards("Jack", "Clubs", 10, 15),
 
-            Cards("10", "kier", 10, 16),
-            Cards("10", "karo", 10, 17),
-            Cards("10", "pik", 10, 18),
-            Cards("10", "trefl", 10, 19),
+            Cards("10", "Hearts", 10, 16),
+            Cards("10", "Diamonds", 10, 17),
+            Cards("10", "Spades", 10, 18),
+            Cards("10", "Clubs", 10, 19),
 
-            Cards("9", "kier", 10, 20),
-            Cards("9", "karo", 10, 21),
-            Cards("9", "pik", 10, 22),
-            Cards("9", "trefl", 10, 23),
+            Cards("9", "Hearts", 9, 20),
+            Cards("9", "Diamonds", 9, 21),
+            Cards("9", "Spades", 9, 22),
+            Cards("9", "Clubs", 9, 23),
 
-            Cards("8", "kier", 8, 24),
-            Cards("8", "karo", 8, 25),
-            Cards("8", "pik", 8, 26),
-            Cards("8", "trefl", 8, 27),
+            Cards("8", "Hearts", 8, 24),
+            Cards("8", "Diamonds", 8, 25),
+            Cards("8", "Spades", 8, 26),
+            Cards("8", "Clubs", 8, 27),
 
-            Cards("7", "kier", 7, 28),
-            Cards("7", "karo", 7, 29),
-            Cards("7", "pik", 7, 30),
-            Cards("7", "trefl", 7, 31),
+            Cards("7", "Hearts", 7, 28),
+            Cards("7", "Diamonds", 7, 29),
+            Cards("7", "Spades", 7, 30),
+            Cards("7", "Clubs", 7, 31),
 
-            Cards("6", "kier", 6, 32),
-            Cards("6", "karo", 6, 33),
-            Cards("6", "pik", 6, 34),
-            Cards("6", "trefl", 6, 35),
+            Cards("6", "Hearts", 6, 32),
+            Cards("6", "Diamonds", 6, 33),
+            Cards("6", "Spades", 6, 34),
+            Cards("6", "Clubs", 6, 35),
 
-            Cards("5", "kier", 5, 36),
-            Cards("5", "karo", 5, 37),
-            Cards("5", "pik", 5, 38),
-            Cards("5", "trefl", 5, 39),
+            Cards("5", "Hearts", 5, 36),
+            Cards("5", "Diamonds", 5, 37),
+            Cards("5", "Spades", 5, 38),
+            Cards("5", "Clubs", 5, 39),
 
-            Cards("4", "kier", 4, 40),
-            Cards("4", "karo", 4, 41),
-            Cards("4", "pik", 4, 42),
-            Cards("4", "trefl", 4, 43),
+            Cards("4", "Hearts", 4, 40),
+            Cards("4", "Diamonds", 4, 41),
+            Cards("4", "Spades", 4, 42),
+            Cards("4", "Clubs", 4, 43),
 
-            Cards("3", "kier", 3, 44),
-            Cards("3", "karo", 3, 45),
-            Cards("3", "pik", 3, 46),
-            Cards("3", "trefl", 3, 47),
+            Cards("3", "Hearts", 3, 44),
+            Cards("3", "Diamonds", 3, 45),
+            Cards("3", "Spades", 3, 46),
+            Cards("3", "Clubs", 3, 47),
 
-            Cards("2", "kier", 3, 48),
-            Cards("2", "karo", 3, 49),
-            Cards("2", "pik", 3, 50),
-            Cards("2", "trefl", 3, 51)} ;
+            Cards("2", "Hearts", 2, 48),
+            Cards("2", "Diamonds", 2, 49),
+            Cards("2", "Spades", 2, 50),
+            Cards("2", "Clubs", 2, 51)} ;
 };
 
 #endif //CARDS_HPP
