c4ba8459f2117139aa498e4104049bd02a4b6f02
adam147g <87074335+adam147g@users.noreply.github.com>
2022-06-08 10:27:05 +0200

    Update main.cpp

--- cleaning_function/main.cpp
+++ cleaning_function/main.cpp
@@ -1,13 +1,8 @@
-#include "cleaning.hpp"
-
-void cleaning_function(Croupier& croupier, Player& player) {
-    show_current_status(croupier, player)
-    croupier.clear_table
-    croupier.clear_cards
-    player.clear_cards
-    continue
-}
-void show_current_status(Croupier& croupier, Player& player) {
-    croupier.show_cards();
-    player.show_cards();
+void cleaning_function(Croupier &croupier, Player &player, bool *need_to_shuffle, Hand *hand) {
+    croupier.clean_hand();
+    player.clean_hand();
+    if (*need_to_shuffle) {
+        Hand new_hand;
+        *hand = new_hand;
+    }
 }
